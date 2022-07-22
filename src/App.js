ce14c75232be496283488c196a2b52450de9473b
William Richardson <majorrapp7@gmail.com>
2022-07-22 14:31:14 -0400

    complete sprint challenge

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import './App.css';
+import axios from 'axios'
+import Character from './components/Character'
+
 
 const App = () => {
+
+  const [starwarsCharacter, setstarwarsCharacter] = useState([])
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
 
@@ -8,9 +14,27 @@ const App = () => {
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
+    useEffect(() => {
+    axios.get('https://swapi.dev/api/people/')
+      .then(res => {
+        setstarwarsCharacter(res.data)
+      })
+      .catch(err => {
+        console.log(err)
+      })
+  }, [])
+
+  console.table(starwarsCharacter)
+  const Character = starwarsCharacter.map(inner => inner.name + inner.height + inner.mass + inner.hair_color + inner.skin_color)
+
+  console.log(Character)
+
+
+
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+      {Character}
     </div>
   );
 }
